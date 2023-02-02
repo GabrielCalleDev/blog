4c07d9f6c820f0e12064f707f7a6be4547e279c8
Gabriel Calle <gabriel.calle92@gmail.com>
2023-02-02 12:32:36 +0100

    Creando vistas para el blog

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de inicio - POSTS</title>
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <h1>Pagina para mostrar los posts.</h1>
+</body>
+</html>
