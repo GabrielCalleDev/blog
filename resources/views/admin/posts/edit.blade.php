e3ee5704354b8c3475fd53a7f4c31523cadbfd8d
Gabriel Calle <gabriel.calle92@gmail.com>
2023-02-07 10:40:24 +0100

    update option select en editar post

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -23,9 +23,9 @@
                     <input type="text" class="form-control mb-2" name="post"  id="title" value="{{ old('post', $post->post) }}">
                     <label for="category-id" class="mb-2">Categoría</label>
                     <select class="form-control" name="category_id" id="category-id">
-                        <option value="{{ old('category_id', $post->category_id) }}">{{ $categories->firstWhere('id', $post->category_id)->category_name }}</option>
+                        <option value=""></option>
                         @foreach ($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                            <option value="{{ $category->id }}" <?= $category->id == $post->category_id ? 'selected':'' ?> >{{ $category->category_name }}</option>
                         @endforeach
                     </select>
                     <label for="content" class="mb-2">Contenido</label>
