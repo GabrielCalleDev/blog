3078df85fdc82cb9f91d75647c46984911437895
Gabriel Calle <gabriel.calle92@gmail.com>
2023-02-10 11:53:34 +0100

    bloqueo de /register solo para administradores

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
+// use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -38,7 +38,8 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('auth');
+        $this->middleware('role:admin');
     }
 
     /**
@@ -47,6 +48,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+   
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -69,5 +71,16 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+}
+
+
+trait RegistersUsers
+{
+    public function showRegistrationForm()
+    {
+        return to_route('admin.users.new');
+
+        // return view('auth.register');
     }
 }
